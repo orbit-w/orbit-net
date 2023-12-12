65ebe7356d95b825aaff9eed46a2155d869ee08b
orbit <orbit@orbitMacBook-Pro.local>
2023-12-12 18:49:43 +0800

    add:IStreamClient desc

--- core/stream_transport/stream.go
+++ core/stream_transport/stream.go
@@ -15,7 +15,7 @@ type IStreamClient interface {
 	Recv() (packet.IPacket, error)
 	Send(data packet.IPacket) error
 	// CloseSend closes the send direction of the stream. It closes the stream
-	// when non-nil error is met. It is also not safe to call CloseSend
+	// when non-nil transport_err is met. It is also not safe to call CloseSend
 	// concurrently with SendMsg.
 	CloseSend() error
 }
